7e56a8b827186113ae7f7b509536410d6e0b5915
daring <danila.daring@gmail.com>
2024-03-25 17:46:43 +0300

    add graalvm-native gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.graalvm.buildtools.native") version "0.10.1"
 }
 
 group = "io.github.daring2"
@@ -11,6 +12,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            mainClass.set("HelloWorld")
+        }
+    }
 }
 
 tasks.test {
